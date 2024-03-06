fd66d05333069c00d02a2e6d5717c852070212fa
galencar1 <gabrielfalencar@gmail.com>
2024-03-05 22:51:34 -0300

    Criando controller de insercao de cliente

--- src/main/java/com/gfalencar/hexagonal/application/core/usecase/InsertCustomerUseCase.java
+++ src/main/java/com/gfalencar/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -1,10 +1,11 @@
 package com.gfalencar.hexagonal.application.core.usecase;
 
 import com.gfalencar.hexagonal.application.core.domain.Customer;
+import com.gfalencar.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.gfalencar.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
 import com.gfalencar.hexagonal.application.ports.out.InsertCustomerOutputPort;
 
-public class InsertCustomerUseCase {
+public class InsertCustomerUseCase implements InsertCustomerInputPort {
 
     private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
     private final InsertCustomerOutputPort insertCustomerOutputPort;
@@ -16,7 +17,7 @@ public class InsertCustomerUseCase {
         this.insertCustomerOutputPort = insertCustomerOutputPort;
     }
 
-
+    @Override
     public void insert(Customer customer, String zipCode) {
         var address = findAddressByZipCodeOutputPort.find(zipCode);
         customer.setAddress(address);
